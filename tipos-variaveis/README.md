2edf37178bb3368fd8b9eee3f8d6245716c1b5cd
Jabneel Pereira <jabneel_pereira@hotmail.com>
2024-07-15 16:38:46 -0300

    Mais alguns aprendizados sobre as variáveis!

--- tipos-variaveis/README.md
+++ tipos-variaveis/README.md
@@ -3,3 +3,12 @@
 No Java, existem algumas palavras reservadas, para a representação dos tipos de dados básicos, que precisam ser manipulados, para a construção de programas. Estes tipos de dados são conhecidos como tipos primitivos (Primitive Types).
 
 > Os oito tipos primitivos em Java são: int, byte, short, long, float, double, boolean e char – esses tipos não são considerados objetos e portanto representam valores brutos. Eles são armazenados diretamente na pilha de memória. (Memory stack)
+
+### Oque apredi nesse diretório!
+
+Busquei entender aqui a utilização das variaves do tipo inteiro e com partes fracionárias.
+Observei que o tipo long precisa terminar com L, o tipo float precisa terminar com F.
+
+Atualmente, com os computadores modernos, se tornou desnecessário utilizar os tipos short e byte, pois não precisamos nos preocupar tanto assim com o espaço de memória reduzido.
+
+Posso realizar auteração das varival em qual quer momento da execução do código. Mais caso utilize o final a variavel se torna uma constante e não podera ser alterada durante toda a execução do código.
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,7 +4,7 @@ public class TiposVariaveis {
 
         int idade; // Tipo "int", nome "idade", com nenhum valor atribuído.
         int anoFabricacao = 2021; // tipo "int", nome "anoFabricacao", com valor 2021.
-        double salarioMinimo = 2.500; // tipo "double", nome "salarioMinimo", valor 2.500.
+        double salarioMinimo = 2500; // tipo "double", nome "salarioMinimo", valor 2.500.
 
         idade = 123;
         short ano = 2021;
@@ -12,6 +12,19 @@ public class TiposVariaveis {
         long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
         float pi = 3.14F;
         double salario = 1275.33;
+
+        System.out.printf("Salario atual é: " + salario + "\n");
+
+        // Posso realizar auteração da varival em qual quer momento da execução do
+        // código
+        salario = 1435.26;
+
+        System.out.printf("Salario atual é: " + salario);
+
+        // O final é usado para a não alteração da variavel durante toda a execução do
+        // código
+
+        final double VALOR_DE_pi = 3.14;
 
         /*
          * Busquei entender aqui a utilização das variaves do tipo inteiro e com partes
